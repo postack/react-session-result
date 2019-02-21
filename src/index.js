bcef5a378305e61dc05bca010fe6ecf04ed8aaba
Maxi Aragon <jose.maximiliano.aragon@gmail.com>
2019-02-21 16:52:03 -0300

    nuevos datos a mostrar

--- src/index.js
+++ src/index.js
@@ -92,27 +92,48 @@ class SessionResult extends Component {
       nick,
       avatarUrl,
     } = this.props
+    const y90 = getHeartsByEffort(90, maxHeartbeat, minHeartbeat);
+    const y80 = getHeartsByEffort(80, maxHeartbeat, minHeartbeat);
+    const y70 = getHeartsByEffort(70, maxHeartbeat, minHeartbeat);
+    const y60 = getHeartsByEffort(60, maxHeartbeat, minHeartbeat);
     let lastValid = 0;
-    const { data, e90, e80, e70, e60 } = currentData.reduce((all, curr, index) => {
-      lastValid = curr > 40 ? curr : lastValid;
-      all.data.push({
-        x: index,
-        y: !curr || curr < 40 ? lastValid : curr
+    const { data, e90, e80, e70, e60, time90toMore, time80to90, time70to80, time60to70, timeUnder60 } = currentData
+      .reduce((all, curr, index) => {
+        lastValid = curr > 40 ? curr : lastValid;
+        all.data.push({
+          x: index,
+          y: !curr || curr < 40 ? lastValid : curr
+        })
+        if(index === currentData.length-1 || index === 0) {
+          all.e90.push({ x: index, y: y90 })
+          all.e80.push({ x: index, y: y80 })
+          all.e70.push({ x: index, y: y70 })
+          all.e60.push({ x: index, y: y60 })
+        }
+        if(curr < y60) {
+          all.timeUnder60 += 1
+        }else if(curr < y70) {
+          all.time60to70 += 1
+        }else if(curr < y80) {
+          all.time70to80 += 1
+        }else if(curr < y90) {
+          all.time80to90 += 1
+        }else {
+          all.time90toMore += 1
+        }
+        return all 
+      }, {
+        data: [],
+        e80: [],
+        e90: [],
+        e70: [],
+        e60: [],
+        time90toMore: 0,
+        time80to90: 0,
+        time70to80: 0,
+        time60to70: 0,
+        timeUnder60: 0,
       })
-      if(index === currentData.length-1 || index === 0) {
-        all.e90.push({ x: index, y: getHeartsByEffort(90, maxHeartbeat, minHeartbeat)})
-        all.e80.push({ x: index, y: getHeartsByEffort(80, maxHeartbeat, minHeartbeat)})
-        all.e70.push({ x: index, y: getHeartsByEffort(70, maxHeartbeat, minHeartbeat)})
-        all.e60.push({ x: index, y: getHeartsByEffort(60, maxHeartbeat, minHeartbeat)})
-      }
-      return all 
-    }, {
-      data: [],
-      e80: [],
-      e90: [],
-      e70: [],
-      e60: []
-    })
     return {
       dateSession,
       points,
@@ -124,7 +145,12 @@ class SessionResult extends Component {
       max: maxHeartbeat,
       basal: minHeartbeat,
       avatarUrl: avatarUrl,
-      nick: nick
+      nick: nick,
+      time90toMore, 
+      time80to90, 
+      time70to80, 
+      time60to70, 
+      timeUnder60,
     }
   }
 
@@ -145,120 +171,133 @@ class SessionResult extends Component {
       basal,
       avatarUrl,
       nick,
-      id
+      time90toMore, 
+      time80to90, 
+      time70to80, 
+      time60to70, 
+      timeUnder60,
     } = this.getFormattedSession()
     const axisY = [basal - 8, max + 8]
     const ticksEffort = [60,70,80,90]
-            
+    const { id } = this.props;
     return (
-      <svg id={id} style={styles.parent} viewBox={`0 0 ${width+marginWidth} ${height+marginHeight}`}
-        key={dateSession}
-      >
-        <rect x={width-marginWidth/2} y={marginHeight/4} width={marginHeight/2} height={marginHeight/4} fill="#727272"/>
-        <rect x={0} y={marginHeight/4} width={marginHeight/2} height={marginHeight/4}  fill="#727272"/>
-        <VictoryLabel x={marginWidth/2} y={25} style={styles.title}
-          text={`${moment(dateSession).format('DD/MM/YYYY HH:mm')}. ${nick} DEP: ${points}`}
-        />
-        <VictoryLabel x={width-marginWidth/2 + 10} y={marginHeight/4 + LEGEND_FONT_SIZE} style={styles.legend}
-          text="EFF%"
-        />
-        <VictoryLabel x={10} y={marginHeight/4 + LEGEND_FONT_SIZE} width={marginHeight/2} style={styles.legend}
-          text="PPM"
-        />
-        
-        {ORANGES.map((o, i) => (
-          <rect 
-            key={i}
-            x={marginWidth/2} 
-            y={marginHeight/2+(height/10)*(i)} 
-            width={width-marginWidth} 
-            height={height/10} 
-            fill={o}/>
-          ))
-        }
-        <VictoryAxis
-          width={width}
-          height={height}
-          offsetY={-marginHeight/2}
-          domain={[0, data.length]}
-          standalone={false}
-          tickValues={getTimeTicks(data)}
-          tickFormat={(t) => `${Math.floor(t/60)}'`}
-          style={styles.axisPrincipal}
-        />
-        
-        <VictoryAxis dependentAxis
-          width={width}
-          //offsetX={marginWidth/2}
-          orientation="left"
-          height={height+marginHeight}
-          domain={axisY}
-          standalone={false}
-          style={styles.axisPrincipal}
-        />
+      <div>
+        <svg id={id} style={styles.parent} viewBox={`0 0 ${width+marginWidth} ${height+marginHeight}`}
+          key={dateSession}
+        >
+          <rect x={width-marginWidth/2} y={marginHeight/4} width={marginHeight/2} height={marginHeight/4} fill="#727272"/>
+          <rect x={0} y={marginHeight/4} width={marginHeight/2} height={marginHeight/4}  fill="#727272"/>
+          <VictoryLabel x={marginWidth/2} y={25} style={styles.title}
+            text={`${moment(dateSession).format('DD/MM/YYYY HH:mm')}. ${nick} DEP: ${points}`}
+          />
+          <VictoryLabel x={width-marginWidth/2 + 10} y={marginHeight/4 + LEGEND_FONT_SIZE} style={styles.legend}
+            text="EFF%"
+          />
+          <VictoryLabel x={10} y={marginHeight/4 + LEGEND_FONT_SIZE} width={marginHeight/2} style={styles.legend}
+            text="PPM"
+          />
+          
+          {ORANGES.map((o, i) => (
+            <rect 
+              key={i}
+              x={marginWidth/2} 
+              y={marginHeight/2+(height/10)*(i)} 
+              width={width-marginWidth} 
+              height={height/10} 
+              fill={o}/>
+            ))
+          }
+          <VictoryAxis
+            width={width}
+            height={height}
+            offsetY={-marginHeight/2}
+            domain={[0, data.length]}
+            standalone={false}
+            tickValues={getTimeTicks(data)}
+            tickFormat={(t) => `${Math.floor(t/60)}'`}
+            style={styles.axisPrincipal}
+          />
+          
+          <VictoryAxis dependentAxis
+            width={width}
+            //offsetX={marginWidth/2}
+            orientation="left"
+            height={height+marginHeight}
+            domain={axisY}
+            standalone={false}
+            style={styles.axisPrincipal}
+          />
 
-        <VictoryAxis dependentAxis
-          width={width}
-          offsetX={marginWidth/2}
-          orientation="right"
-          height={height+marginHeight}
-          domain={axisY}
-          standalone={false}
-          tickValues={[e60[0].y, e70[0].y, e80[0].y, e90[0].y]}
-          tickFormat={(t, i) => `${ticksEffort[i]}%`}
-          style={styles.axisPrincipal}
-        />
-        
-        
-        <VictoryLine
-          data={data}
-          width={width}
-          height={height+marginHeight}
-          domain={{ x: [0, data.length], y: axisY }}
-          //scale={{x: "time", y: "linear"}}
-          standalone={false}
-          style={styles.dataLine}
-        /> 
-        
+          <VictoryAxis dependentAxis
+            width={width}
+            offsetX={marginWidth/2}
+            orientation="right"
+            height={height+marginHeight}
+            domain={axisY}
+            standalone={false}
+            tickValues={[e60[0].y, e70[0].y, e80[0].y, e90[0].y]}
+            tickFormat={(t, i) => `${ticksEffort[i]}%`}
+            style={styles.axisPrincipal}
+          />
+          
+          
+          <VictoryLine
+            data={data}
+            width={width}
+            height={height+marginHeight}
+            domain={{ x: [0, data.length], y: axisY }}
+            //scale={{x: "time", y: "linear"}}
+            standalone={false}
+            style={styles.dataLine}
+          /> 
+          
 
 
-        <VictoryLine
-          data={e90}
-          width={width}
-          height={height+marginHeight}
-          domain={{ x: [0, data.length], y: axisY }}
-          //scale={{x: "time", y: "linear"}}
-          standalone={false}
-          style={styles.effortLine}
-        /> 
-        <VictoryLine
-          data={e80}
-          width={width}
-          height={height+marginHeight}
-          domain={{ x: [0, data.length], y: axisY }}
-          //scale={{x: "time", y: "linear"}}
-          standalone={false}
-          style={styles.effortLine}
-        />
-        <VictoryLine
-          data={e70}
-          width={width}
-          height={height+marginHeight}
-          domain={{ x: [0, data.length], y: axisY }}
-          //scale={{x: "time", y: "linear"}}
-          standalone={false}
-          style={styles.effortLine}
-        />
-        <VictoryLine
-          data={e60}
-          width={width}
-          height={height+marginHeight}
-          domain={{ x: [0, data.length], y: axisY }}
-          //scale={{x: "time", y: "linear"}}
-          standalone={false}
-          style={styles.effortLine}
-        />
-      </svg>
+          <VictoryLine
+            data={e90}
+            width={width}
+            height={height+marginHeight}
+            domain={{ x: [0, data.length], y: axisY }}
+            //scale={{x: "time", y: "linear"}}
+            standalone={false}
+            style={styles.effortLine}
+          /> 
+          <VictoryLine
+            data={e80}
+            width={width}
+            height={height+marginHeight}
+            domain={{ x: [0, data.length], y: axisY }}
+            //scale={{x: "time", y: "linear"}}
+            standalone={false}
+            style={styles.effortLine}
+          />
+          <VictoryLine
+            data={e70}
+            width={width}
+            height={height+marginHeight}
+            domain={{ x: [0, data.length], y: axisY }}
+            //scale={{x: "time", y: "linear"}}
+            standalone={false}
+            style={styles.effortLine}
+          />
+          <VictoryLine
+            data={e60}
+            width={width}
+            height={height+marginHeight}
+            domain={{ x: [0, data.length], y: axisY }}
+            //scale={{x: "time", y: "linear"}}
+            standalone={false}
+            style={styles.effortLine}
+          />
+        </svg>
+        <div>
+          <p> <b>Segundos trabajados a más de 90% esfuerzo:</b> {time90toMore} (el {Math.round((time90toMore*10000)/data.length) / 100}% del entrenamiento)</p> 
+          <p> <b>Segundos trabajados entre 80% y 90% de esfuerzo:</b> {time80to90} (el {Math.round((time80to90*10000)/data.length) / 100}% del entrenamiento)</p> 
+          <p> <b>Segundos trabajados entre 70% y 80% de esfuerzo:</b> {time70to80} (el {Math.round((time70to80*10000)/data.length) / 100}% del entrenamiento)</p> 
+          <p> <b>Segundos trabajados entre 60% y 70% de esfuerzo:</b> {time60to70} (el {Math.round((time60to70*10000)/data.length) / 100}% del entrenamiento)</p> 
+          <p> <b>Segundos trabajados a menos de 60% esfuerzo:</b> {timeUnder60} (el {Math.round((timeUnder60*10000)/data.length) / 100}% del entrenamiento)</p> 
+        </div>
+      </div>
     )
   }
 }
